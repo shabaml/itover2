dab8d7ebd127c8ea4b139c3d70f41d8352692c36
Shaba_ml <alexey.shabunya@mail.ru>
2022-07-01 15:17:36 +0300

    add name logic to 1-3que.py

---
+++ 1-3que.py
@@ -0,0 +1,24 @@
+my_list = [42,43]
+my_dict = {
+    'foo': {
+        'a':12,
+        'b': (1, 2, 3, 4, my_list)
+    },
+    'bar': {
+        'c':12,
+        'd': {5, 6, 7, 8}
+    },
+    'moo': {
+        'e' :12,
+        'f' : {'lol': ['l', 'o', 'l'] }
+    },
+}
+
+print(my_dict['foo'])
+print(my_dict['foo']['b'])
+my_list.append(44)
+print(my_list)
+
+
+
+
